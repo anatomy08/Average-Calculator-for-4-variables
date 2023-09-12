a12c66428220f9553c683ab5e6cb38d425d14af0
User <User@MSI>
2023-09-12 15:53:59 +0800

    Add project files.

---
+++ Lesson 5 Average Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Example of Average Calculator. in c#
+
+Console.Write("English    :  ");
+float english = Convert.ToSingle(Console.ReadLine());
+Console.Write("Math       :  ");
+float math = Convert.ToSingle(Console.ReadLine());
+Console.Write("Science    :  ");
+float science = Convert.ToSingle(Console.ReadLine());
+Console.Write("Computer   :  ");
+float computer = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("");
+
+float average = (english + math + science + computer) / 4;
+Console.Write("Average is :  " + average);
+
+
---
+++ b/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Runtime.InteropServices;
+
+Console.Write("English    :  ");
+int english = Convert.ToInt32(Console.ReadLine());
+Console.Write("Math       :  ");
+int math = Convert.ToInt32(Console.ReadLine());
+Console.Write("Science    :  ");
+int science = Convert.ToInt32(Console.ReadLine());
+Console.Write("Computer   :  ");
+float computer = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("");
+
+float average = (english + math + science + computer) / 4;
+Console.Write("Average is :  " + average);
